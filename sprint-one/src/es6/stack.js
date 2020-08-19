e6a28cc9901be1b1913d8c6b09b97ce23ce07a2f
Christopher Sorta <chris.sorta@yahoo.com>
2020-08-19 14:13:29 -0700

    Implement stack class with ES6 pattern

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,28 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.index = 0;
+    this.storage = {};
+  }
+
+  size() {
+    return this.index;
+  }
+
+  push(value) {
+    this.storage[this.index] = value;
+    this.index++;
+  }
+
+  pop () {
+    var popped = this.storage[this.index - 1];
+    delete this.storage[this.index - 1];
+    if (this.index === 0) {
+      this.index = 0;
+    } else {
+      this.index--;
+    }
+    return popped;
   }
 
 }
