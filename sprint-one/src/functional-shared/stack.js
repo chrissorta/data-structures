c910b1b4bc78300960ea5c840f725f62ec8aed7e
Christopher Sorta <chris.sorta@yahoo.com>
2020-08-19 12:24:31 -0700

    Implement stack for functional class style

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance.index = 0;
+  someInstance.storage = {};
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  push: function(value) {
+    this.storage[this.index] = value;
+    this.index++;
+
+  },
+
+  pop: function() {
+    var popped = this.storage[this.index - 1];
+
+
+
+    return popped;
+
+  },
+
+  size: function() {
+    return this.index;
+  }
+
+
+};
 
 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,29 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+
+
+
+  var index = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.pop = function() {
+    var popped = storage[index - 1];
+    delete storage[index - 1];
+    if (index === 0) {
+      index = 0;
+    } else {
+      index--;
+    }
+    return popped;
   };
 
   someInstance.size = function() {
+    return index;
   };
 
   return someInstance;
