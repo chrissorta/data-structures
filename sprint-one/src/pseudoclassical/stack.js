0aadf3817840d60b41af310556aa8d01e6dd2e4d
Christopher Sorta <chris.sorta@yahoo.com>
2020-08-19 13:58:38 -0700

    Implement prototypal and psuedoclassical classes stack classes

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.index = 0;
+  this.storage = {};
 };
 
+Stack.prototype.size = function() {
+  return this.index;
+};
+
+Stack.prototype.push = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+Stack.prototype.pop = function() {
+  var popped = this.storage[this.index - 1];
+  delete this.storage[this.index - 1];
+  if (this.index === 0) {
+    this.index = 0;
+  } else {
+    this.index --;
+  }
+  return popped;
+};
 
