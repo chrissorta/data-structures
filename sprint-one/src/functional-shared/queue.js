07d432938c2d41bbc5b9a9fa5a36cc0fc8fa20aa
Christopher Sorta <chris.sorta@yahoo.com>
2020-08-19 15:28:48 -0700

    Implement queue class for functional and functional-shared styles

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.removeIndex = 0;
+  someInstance.latestIndex = -1;
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return Object.keys(this.storage).length;
+  },
+
+  enqueue: function(value) {
+    this.storage[this.latestIndex + 1] = value;
+    this.latestIndex++;
+  },
+
+  dequeue: function() {
+    var dequeued = this.storage[this.removeIndex];
+    delete this.storage[this.removeIndex];
+    this.removeIndex++;
+    return dequeued;
+
+  }
+
+
+};
 
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,18 +2,26 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+  var removeIndex = 0;
+  var latestIndex = -1;
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-
+    storage[latestIndex + 1] = value;
+    latestIndex++;
   };
 
   someInstance.dequeue = function() {
+    var dequeued = storage[removeIndex];
+    delete storage[removeIndex];
+    removeIndex++;
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
 
   };
 
